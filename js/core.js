e4ad127c483e58c391f887d420f58d86af4d0f53
Kostya Ostapuk <ostapuk97@gmail.com>
2017-12-16 22:38:29 +0300

    first upload

---
+++ js/core.js
@@ -0,0 +1,55 @@
+var country = ["China", "India", "United State", "Indonesia", "Brazil",
+  "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan"
+];
+var countryInput = document.getElementById("countryName");
+var countryList = document.getElementById("list");
+var dropdown = document.getElementsByClassName("dropdown");
+var clicked = false;
+
+
+function search() {
+  var countryVal = countryInput.value;
+  clearList();
+  for (var i = 0; i < country.length; i++) {
+    if (country[i].toLowerCase().search(countryVal) != -1) {
+      countryList.innerHTML += "<li id='" + country[i] + "'>" + country[i] + "</li>";
+    }
+  }
+  clicked = true;
+}
+function showList() {
+  if (clicked == false) {
+    for (var i = 0; i < country.length; i++) {
+      countryList.innerHTML += "<li id='" + country[i] + "'>" + country[i] + "</li>";
+    }
+    clicked = true;
+  }
+}
+function clearList() {
+		countryList.innerHTML = "";
+	  clicked = false;
+}
+function checkClicked() {
+  function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement;
+  }
+  countryList.onclick = function(event) {
+    var target = getEventTarget(event);
+    countryInput.value=target.innerHTML;
+    clearList();
+  };
+}
+
+checkClicked();
+countryInput.oninput=function(){
+	search();
+};
+countryInput.addEventListener("click", showList);
+dropdown.onclick=function(){
+	dropdown.focus();
+};
+dropdown.onblur=function(){
+	countryInput.innerHTML="";
+	clearList();
+};
